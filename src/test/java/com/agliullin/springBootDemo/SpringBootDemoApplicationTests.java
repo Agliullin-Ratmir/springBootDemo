249693b86fb52e59b3948b9e6e5f085edd9eaf6e
Agliullin-Ratmir <Cerf12rfrjuj10>
2020-07-01 12:13:01 +0300

    Task 11: lambda, optional.

--- src/test/java/com/agliullin/springBootDemo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/agliullin/springBootDemo/SpringBootDemoApplicationTests.java
@@ -9,8 +9,11 @@ import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.parameters.P;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,7 +28,7 @@ class SpringBootDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		Person person = getPerson();
+		Person person = getPerson(LOGIN, PASSWORD, Role.USER);
 		service.addNewPerson(person);
 
 		List<Person> list = service.findAll();
@@ -37,12 +40,52 @@ class SpringBootDemoApplicationTests {
 		assertEquals(Role.USER, person.getRole());
 	}
 
-	private Person getPerson() {
-		Person person = new Person();
-		person.setLogin(LOGIN);
-		person.setPassword(PASSWORD);
-		person.setRole(Role.USER);
-		return person;
+	@Test
+	public void JLTest() {
+		saveSquad(getSquad());
+
+		List<Person> squad = null;
+		assertFalse(isSquadFilled(squad));
+
+		squad = service.findAll();
+		assertTrue(isSquadFilled(squad));
+
+		Long adminsAmount = squad.stream().filter(person -> Role.ADMIN.equals(person.getRole())).count();
+		Long usersAmount = squad.stream().filter(person -> Role.USER.equals(person.getRole())).count();
+		assertEquals(2, adminsAmount);
+		assertEquals(5, usersAmount);
 	}
 
+	private boolean isSquadFilled(List<Person> list) {
+		Optional<List<Person>> listOptional = Optional.ofNullable(list);
+		return listOptional.isPresent();
+	}
+
+	private void saveSquad(List<Person> list) {
+		list.stream().forEach(person -> {
+			service.addNewPerson(person);
+			System.out.println(String.format("Person %s has been added",
+					person.getLogin()));
+		});
+	}
+
+	private List<Person> getSquad() {
+		List<Person> list = new ArrayList<>();
+		list.add(getPerson("Batman", "pass1", Role.ADMIN));
+		list.add(getPerson("Superman", "pass2", Role.ADMIN));
+		list.add(getPerson("Flash", "pass3", Role.USER));
+		list.add(getPerson("Lantern", "pass4", Role.USER));
+		list.add(getPerson("Wonderwoman", "pass5", Role.USER));
+		list.add(getPerson("Cyborg", "pass6", Role.USER));
+		list.add(getPerson("Hunter", "pass7", Role.USER));
+		return list;
+	}
+
+	private Person getPerson(String login, String password, Role role) {
+		Person person = new Person();
+		person.setLogin(login);
+		person.setPassword(password);
+		person.setRole(role);
+		return person;
+	}
 }
